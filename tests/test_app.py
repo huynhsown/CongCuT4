613dee5ef16e410790a92d832ddfab93c50ba6ba
huynhsown <116697096+huynhsown@users.noreply.github.com>
2025-04-16 04:07:56 +0000

    SCRUM-5

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+def test_example():
+    assert 1 + 1 == 2
+
+def test_example():
+    assert True
+
+def test_home():
+    assert True
+def test_about():
+    assert True
+
+def test_sleep():
+    assert True
